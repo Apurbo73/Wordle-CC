8211a6bc31cc79321cfec88943225075a58ef022
Apurbo73 <apurbodebnath895@gmail.com>
2025-06-02 00:35:04 +0600

    done

---
+++ a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        string S, T;
+        cin >> S >> T;
+        string M = "";
+
+        for (int i = 0; i < 5; i++) {
+            if (S[i] == T[i]) {
+                M += 'G';
+            } else {
+                M += 'B';
+            }
+        }
+
+        cout << M << endl;
+    }
+
+    return 0;
+}
